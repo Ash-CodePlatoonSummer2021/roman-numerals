962757665d6b51416fd8988c4926efca6e624bfa
asriniv0 <ashsrinivasan1211@gmail.com>
2021-05-25 13:04:17 -0400

    Roman Numerals 4000

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,13 @@
 exports.toRoman = function(num) {
-
+    var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var symbols = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+    var output=''
+    for(var i=0;i<13;i++){
+      var quotient = Math.floor(num/numbers[i]);
+      num=num%numbers[i];
+      for(var j=0;j<quotient;j++){
+        output+=symbols[i];
+      }
+    }  
+    return output; 
 };
