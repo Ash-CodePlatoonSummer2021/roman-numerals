ada16dd35dffbefe9e03742505f465d9830ea0df
asriniv0 <ashsrinivasan1211@gmail.com>
2021-05-26 14:56:15 -0400

    python roman

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,12 @@
+import math
 def to_roman(num):
     # write your code here!
+    numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    symbols = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+    output=''
+    for x in range(13):
+      quo = math.floor(num/numbers[x])
+      num=num%numbers[x]
+      for y in range(quo):
+        output+=symbols[x]
+    return output
